fa059d18cebf4df188078ff49d3bec9d60293e49
Franco Alemandi <team@platzi.com>
2020-07-10 22:49:25 -0300

    Resume added, still looking for details

--- my-own-page/src/components/Resume.vue
+++ my-own-page/src/components/Resume.vue
@@ -3,15 +3,15 @@
     <div class="container">
       <div class="columns is-centered">
         <div class="column is-one-quarter">
-          <h2 class="is-size-4">Resume</h2>
+          <h2 class="is-size-4">RESUME</h2>
         </div>
         <div class="column is-one-third">
-          <a href="#">Resume.pdf</a>
+          <a class="anchor-class" href="#">Resume.pdf</a>
         </div>
       </div>
       <div class="columns is-centered">
         <div class="column is-one-quarter">
-          <h2 class="is-size-4">Profile</h2>
+          <h2 class="is-size-4">PROFILE</h2>
         </div>
         <div class="column is-one-third is-resume">
           <h2 class="is-size-4">I’m a front-end developer specializing in crafting engaging digital experiences for web and mobile applications.</h2><br />
@@ -19,7 +19,8 @@
           I’m passionate about turning great ideas into exciting, functional, accessible digital products. With seven years of experience in the industry, I’ve built various products for individuals and organizations ranging from simple, highly converting marketing landing pages to mid-sized Node.js web applications. <br /><br />
           As a freelancer since 2013, I’ve worked with a variety of clients and creative agencies. At the moment, I collaborate primarily with Hub & Spoke Creative to create digital experiences that effectively connect with their audiences. While I’m primarily a front-end developer, in the past I’ve worked with companies and organizations such as Proper Channel and Technole in public relations, marketing and technical writing capacities.
           <br/><br /> When I’m not in a code editor, I typically spend time working on my writing, filming, catching up on some of my most watched shows, and seeing what’s new on my favorite YouTube channel.
-          Interested in working together? Let’s connect.</p>
+          <br /><br />
+          <a class="anchor-class">Interested in working together? Let’s connect.</a></p>
         </div>
       </div>
       <div class="columns">
@@ -28,19 +29,78 @@
       <div class="columns">
         <div class="column"></div>
       </div>
-      <div class="columns is-centered">
-        <div class="column is-one-quarter">
-          <h2 class="is-size-4">Skillset</h2>
+    <div class="columns is-centered">
+      <div class="column is-one-quarter">
+        <h2 class="is-size-4">SKILLSET</h2>
+      </div>
+      <div class="column is-one-third">
+        <div class="container is-size-4" style="position: relative; left: -12px; top: -15px;">Design</div>
+          <div class="image-container">
+            <div class="card-image" style="position: relative; left: 3px;">
+              <figure class="image is-64x64">
+                <a href="https://www.adobe.com/la/products/photoshop.html" target="_blank"><img src="@/assets/images/PS.svg"></a>
+              </figure>
+            </div>
+          </div>
         </div>
-        <div class="column is-one-third">
-          <div class="container is-size-4">Design</div>
-            <div class="image-container">
-              <div class="card-image" style="position: relative; left: 3px;">
-                <figure class="image is-96x96">
-                  <img src="@/assets/images/PS.svg">
-                  <p class="is-size-5 is-centered">Photoshop</p>
-                </figure>
-              </div>
+      </div>
+    </div>
+    <div class="columns is-centered mt-4">
+      <div class="column is-one-quarter"></div>
+      <div class="column is-one-third">
+        <div class="container is-size-4">
+          Code
+        </div>
+      </div>
+    </div>
+    <div class="columns is-centered" style="position: relative; left: -5px;">
+      <div class="column is-3"></div>
+      <div class="column is-1">
+        <div class="image-container">
+          <div class="card-image" style="position: relative; left: 3px;">
+            <figure class="image is-96x96" style="position: relative; top: -10px;">
+              <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><img src="@/assets/images/HTMlvector.svg"></a>
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div class="column is-1">
+        <div class="image-container">
+          <div class="card-image" style="position: relative; left: 3px;">
+            <figure class="image is-64x64">
+              <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><img src="@/assets/images/css.svg"></a>
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div class="column is-1">
+        <div class="image-container">
+          <div class="card-image">
+            <figure class="image is-64x64">
+              <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img src="@/assets/images/JS.svg"></a>
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div class="column is-1">
+        <div class="image-container">
+          <div class="card-image">
+            <figure class="image is-64x64">
+              <a href="https://vuejs.org/" target="_blank"><img src="@/assets/images/Vue.svg"></a>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="columns is-centered" style="position: relative; left: 20px;">
+      <div class="column is-one-quarter"></div>
+      <div class="column is-one-third">
+        <div class="container is-size-4" style="position: relative; left: -12px; top: -15px;">Tools</div>
+          <div class="image-container">
+            <div class="card-image" style="position: relative; left: 3px;">
+              <figure class="image is-64x64">
+                <a href="https://git-scm.com/" target="_blank"><img src="@/assets/images/git.svg" ></a>
+              </figure>
             </div>
           </div>
         </div>
@@ -63,20 +123,19 @@ main {
 }
 figure:hover{
   transition: 0.5s;
-  transform: scale(1.05);
+  transform: scale(1.25);
   cursor: pointer;
 }
-a {
+.anchor-class {
   color: rgb(136, 34, 34);
-  text-transform: uppercase;
   text-decoration: none;
-  letter-spacing: 0.15em;
+  letter-spacing: 0.05em;
   
   display: inline-block;
-  padding: 15px 20px;
+  padding: 5px;
   position: relative;
 }
-a:after {    
+.anchor-class:after {    
   background: none repeat scroll 0 0 transparent;
   bottom: 0;
   content: "";
@@ -88,7 +147,7 @@ a:after {
   transition: width 0.3s ease 0s, left 0.3s ease 0s;
   width: 0;
 }
-a:hover:after { 
+.anchor-class:hover:after { 
   width: 100%; 
   left: 0; 
 }
